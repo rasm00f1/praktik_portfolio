e07cca5daef850d7a6eea1b57e75e4220d1fb3c4
rasmus petersen <rasm00f1@stud.kea.dk>
2021-06-14 07:33:11 +0200

    improving projects

--- src/App.js
+++ src/App.js
@@ -28,15 +28,22 @@ function App() {
   const projectsCopy = [...projects];
   console.log(projectsCopy);
   return (
-    <div className="App" style={{ overflowY: "hidden", scrollBehavior: "smooth", height: "100vh" }}>
-      <section id="hub" style={{ height: "100vh", paddingTop: "5vh" }}>
-        <Hub style={{ maxHeight: "90vh" }}></Hub>
+    <div className="App" style={{ overflowY: "scroll", scrollBehavior: "smooth", height: "100vh" }}>
+      <section id="hub" style={{ overflowX: "scroll", height: "100vh", paddingTop: "5vh" }}>
+        <Hub style={{ margin: "0 auto", height: "90vh" }}></Hub>
       </section>
-      <section id="projects">
-        <nav style={{ height: "10vh", display: "flex", justifyContent: "center", alignItems: "center", position: "sticky", top: "0" }}>
-          <a href="#hub">Hello</a>
+      <section style={{ minHeight: "100vh" }} id="projects">
+        <nav style={{ paddingTop: "5vh", height: "10vh", display: "flex", justifyContent: "center", alignItems: "center", position: "sticky" }}>
+          <a className="link" href="#hub">
+            <svg width="100" height="100" viewBox="0 0 60 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M24.812 16.61L12 2L4.83073 15.9547L24.812 16.61Z" fill="#E27C37" />
+              <path d="M25.0403 15.8955L13.5 1.5L6.07235 15.2656L25.0403 15.8955Z" fill="#3C3466" />
+              <path d="M25.6573 21.3533L24.398 18.965L4.41691 18.7834L6.54374 22.8171L25.6573 21.3533Z" fill="#E27C37" />
+              <path d="M25.8531 20.2666L24.5938 17.8783L5.23184 17.3702L7.35867 21.4038L25.8531 20.2666Z" fill="#3C3466" />
+            </svg>
+          </a>
         </nav>
-        <div style={{ height: "90vh" }} class="m-auto grid grid-cols-2 gap-3 max-w-screen-lg">
+        <div className="grid">
           {projectsCopy.map((project) => (
             <Project isFetched={isFetched} {...project} key={project.title} />
           ))}
--- src/Project.js
+++ src/Project.js
@@ -1,14 +1,48 @@
+import { Disclosure, Transition } from "@headlessui/react";
+import { ReactComponent as Arrow } from "./arrow.svg";
+
 export default function Project(props) {
-  console.log(props);
   return (
-    <article class="shadow-lg">
+    <article style={{ boxShadow: "5px 5px 10px #bebebe,-5px -5px 10px #ffffff", borderRadius: "5px", minWidth: "350px" }}>
       {props.isFetched === true ? (
         <div>
-          <h2>{props.title}</h2>
-          <h3>{props.subtitle}</h3>
+          <a target="blank_" href={props.link}>
+            <div className="go_to_page" style={{ overflowY: "hidden", height: "250px" }}>
+              <img style={{ borderRadius: "5px 5px 0px 0px", objectFit: "cover" }} src={"/img/" + props.img + ".png"} alt={props.img} />
+            </div>
+          </a>
+          <h2 style={{ padding: "0px 10px" }}>{props.title}</h2>
+          <h3 style={{ padding: "0px 0px 15px 15px" }}>{props.subtitle}</h3>
+          <Disclosure>
+            {({ open }) => (
+              <>
+                <Disclosure.Button style={{ width: "96%", margin: "2% 2%" }}>
+                  <div className="disclosure">
+                    <h3>Learn more.</h3>
+                    <Arrow className={`${open ? "transform rotate-180" : ""}`} />
+                  </div>
+                  {/*
+              Use the `open` render prop to rotate the icon when the panel is open
+            */}
+                </Disclosure.Button>
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Disclosure.Panel style={{ paddingLeft: "1rem", paddingBottom: "1rem", width: "350px" }}>
+                    <p>{props.desc}</p>
+                  </Disclosure.Panel>
+                </Transition>
+              </>
+            )}
+          </Disclosure>
         </div>
       ) : (
-        <h1></h1>
+        <h1>sdf</h1>
       )}
     </article>
   );
